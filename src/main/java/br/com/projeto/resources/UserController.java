ea635dce82cdc05c1816b851b98c1de2f274df68
Marcklen <marcklen@icloud.com>
2022-07-20 15:11:23 -0300

    criando o padrao DTO e trabalhando com o model mapper

--- src/main/java/br/com/projeto/resources/UserController.java
+++ src/main/java/br/com/projeto/resources/UserController.java
@@ -1,5 +1,6 @@
 package br.com.projeto.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,18 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projeto.domain.User;
+import br.com.projeto.domain.dto.UserDTO;
 import br.com.projeto.services.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
+	@Autowired
+	private ModelMapper mapper;
+
 	@Autowired
 	private UserService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id) {
-		
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
 }
