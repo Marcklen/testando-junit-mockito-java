9c12850b66410dd8c774590e46e5a859c5e2ca5c
Marcklen <marcklen@icloud.com>
2022-07-21 20:57:35 -0300

    testando validacao de data violation integrity

--- src/test/java/br/com/projeto/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/projeto/services/impl/UserServiceImplTest.java
@@ -3,9 +3,12 @@ package br.com.projeto.services.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -19,14 +22,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import br.com.projeto.domain.User;
 import br.com.projeto.domain.dto.UserDTO;
 import br.com.projeto.repository.UserRepository;
+import br.com.projeto.services.exceptions.DataIntegrityViolationException;
+import br.com.projeto.services.exceptions.ObjectNotFoundException;
 
 @SpringBootTest
 class UserServiceImplTest {
 
-	private static final Integer ID 		= 1;
-	private static final String NAME 		= "Marcklen";
-	private static final String EMAIL 		= "marcklen@email.com";
-	private static final String PASSWORD	= "123";
+	private static final int INDEX = 0;
+	private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado!!!";
+	private static final Integer ID = 1;
+	private static final String NAME = "Marcklen";
+	private static final String EMAIL = "marcklen@email.com";
+	private static final String PASSWORD = "123";
 	
 	@InjectMocks
 	private UserServiceImpl serviceImpl;
@@ -64,18 +71,63 @@ class UserServiceImplTest {
 		assertEquals(ID, response.getId());
 		assertEquals(NAME, response.getName());
 		assertEquals(EMAIL, response.getEmail());
-		
-		//fail("Not yet implemented");
 	}
 
+	//testando metodo find by id porem agora com o uso das exceptiosn para retornar ou nao um objeto
+	@Test
+	void testFindByIdWithObjectNotFoundException() {
+		when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+		try {
+			serviceImpl.findById(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals(OBJETO_NAO_ENCONTRADO, e.getMessage());
+		}
+	}
+	
+	//testando o metodo para achar todos numa lista
 	@Test
 	void testFindAll() {
-		fail("Not yet implemented");
+		when(repository.findAll()).thenReturn(List.of(user));
+		
+		List<User> response = serviceImpl.findAll();
+		
+		assertNotNull(response);
+		assertEquals(1, response.size());		
+		assertEquals(User.class, response.get(INDEX).getClass());
+
+		assertEquals(ID, response.get(INDEX).getId());
+		assertEquals(NAME, response.get(INDEX).getName());
+		assertEquals(EMAIL, response.get(INDEX).getEmail());
+		assertEquals(PASSWORD, response.get(INDEX).getPassword());
 	}
 
 	@Test
 	void testCreate() {
-		fail("Not yet implemented");
+		when(repository.save(any())).thenReturn(user);
+		User response = serviceImpl.create(userDTO);
+		
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(PASSWORD, response.getPassword());
+	}
+	
+	//testando o metodo de criar usuario e verificando se nao ha violacao de dados
+	@Test
+	void testCreateWithDataIntegratyViolationException() {
+		when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+		
+		try {
+			optionalUser.get().setId(2);
+			serviceImpl.create(userDTO);
+		} catch (Exception e) {
+			assertEquals(DataIntegrityViolationException.class, e.getClass());
+			assertEquals("E-mail já cadastrado em nosso sistema!", e.getMessage());
+		}
 	}
 
 	@Test
