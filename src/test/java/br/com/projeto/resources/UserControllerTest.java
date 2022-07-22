2d30a0fb4e1f856b4ef92d6cc0bec4cefe9d88bf
Marcklen <marcklen@icloud.com>
2022-07-22 11:49:45 -0300

    criando metodo para endpoint create

---
+++ src/test/java/br/com/projeto/resources/UserControllerTest.java
@@ -0,0 +1,110 @@
+package br.com.projeto.resources;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import br.com.projeto.domain.User;
+import br.com.projeto.domain.dto.UserDTO;
+import br.com.projeto.services.impl.UserServiceImpl;
+
+@SpringBootTest
+class UserControllerTest {
+
+	private static final String E_MAIL_JA_CADASTRADO_EM_NOSSO_SISTEMA = "E-mail já cadastrado em nosso sistema!";
+	private static final int INDEX = 0;
+	private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado!!!";
+	private static final Integer ID = 1;
+	private static final String NAME = "Marcklen";
+	private static final String EMAIL = "marcklen@email.com";
+	private static final String PASSWORD = "123";
+
+	private User user;
+	private UserDTO userDTO;
+	private Optional<User> optionalUser;
+
+	@InjectMocks
+	private UserController controller;
+	@Mock
+	private ModelMapper mapper;
+	@Mock
+	private UserServiceImpl service;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		startUser();
+	}
+
+	@Test
+	void testFindByIdWithSucess() {
+		when(service.findById(anyInt())).thenReturn(user);
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+
+		ResponseEntity<UserDTO> response = controller.findById(ID);
+
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(UserDTO.class, response.getBody().getClass());
+
+		assertEquals(ID, response.getBody().getId());
+		assertEquals(NAME, response.getBody().getName());
+		assertEquals(EMAIL, response.getBody().getEmail());
+		assertEquals(PASSWORD, response.getBody().getPassword());
+	}
+
+	@Test
+	void testFindAllWithSucessTheReturnListOfUserDTO() {
+		when(service.findAll()).thenReturn(List.of(user));
+		when(mapper.map(any(), any())).thenReturn(userDTO);
+
+		ResponseEntity<List<UserDTO>> response = controller.findAll();
+
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(ArrayList.class, response.getBody().getClass());
+		assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+		
+		assertEquals(ID, response.getBody().get(INDEX).getId());
+		assertEquals(NAME, response.getBody().get(INDEX).getName());
+		assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+		assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
+ 	}
+
+	@Test
+	void testCreate() {
+	}
+
+	@Test
+	void testUpdate() {
+	}
+
+	@Test
+	void testDelete() {
+	}
+
+	private void startUser() {
+		user = new User(ID, NAME, EMAIL, PASSWORD);
+		userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+		optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+	}
+}
