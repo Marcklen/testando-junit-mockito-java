304c1de4ac2f68b9b39d41b5e2aa1a2f14483263
Marcklen <marcklen@icloud.com>
2022-07-22 09:45:38 -0300

    criando metodo para testar o delete sem e com object not found

--- src/test/java/br/com/projeto/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/projeto/services/impl/UserServiceImplTest.java
@@ -2,10 +2,12 @@ package br.com.projeto.services.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -28,27 +30,28 @@ import br.com.projeto.services.exceptions.ObjectNotFoundException;
 @SpringBootTest
 class UserServiceImplTest {
 
+	private static final String E_MAIL_JA_CADASTRADO_EM_NOSSO_SISTEMA = "E-mail já cadastrado em nosso sistema!";
 	private static final int INDEX = 0;
 	private static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado!!!";
 	private static final Integer ID = 1;
 	private static final String NAME = "Marcklen";
 	private static final String EMAIL = "marcklen@email.com";
 	private static final String PASSWORD = "123";
-	
+
 	@InjectMocks
 	private UserServiceImpl serviceImpl;
-	
+
 	@Mock
 	private UserRepository repository;
-	
+
 	@Mock
 	private ModelMapper mapper;
-	
-	//mocando as classes Users
+
+	// mocando as classes Users
 	private User user;
 	private UserDTO userDTO;
 	private Optional<User> optionalUser;
-	
+
 //	@BeforeAll
 //	static void setUpBeforeClass() throws Exception {
 //	}
@@ -59,13 +62,13 @@ class UserServiceImplTest {
 		startUser();
 	}
 
-	//testando o metodo e quando encontrar retornar uma instancia de obj
-	//when find by ID then return an user instance
+	// testando o metodo e quando encontrar retornar uma instancia de obj
+	// when find by ID then return an user instance
 	@Test
 	void testFindById() {
 		when(repository.findById(anyInt())).thenReturn(optionalUser);
 		User response = serviceImpl.findById(ID);
-		
+
 		assertNotNull(response);
 		assertEquals(User.class, response.getClass());
 		assertEquals(ID, response.getId());
@@ -73,7 +76,8 @@ class UserServiceImplTest {
 		assertEquals(EMAIL, response.getEmail());
 	}
 
-	//testando metodo find by id porem agora com o uso das exceptiosn para retornar ou nao um objeto
+	// testando metodo find by id porem agora com o uso das exceptiosn para retornar
+	// ou nao um objeto
 	@Test
 	void testFindByIdWithObjectNotFoundException() {
 		when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
@@ -85,16 +89,16 @@ class UserServiceImplTest {
 			assertEquals(OBJETO_NAO_ENCONTRADO, e.getMessage());
 		}
 	}
-	
-	//testando o metodo para achar todos numa lista
+
+	// testando o metodo para achar todos numa lista
 	@Test
 	void testFindAll() {
 		when(repository.findAll()).thenReturn(List.of(user));
-		
+
 		List<User> response = serviceImpl.findAll();
-		
+
 		assertNotNull(response);
-		assertEquals(1, response.size());		
+		assertEquals(1, response.size());
 		assertEquals(User.class, response.get(INDEX).getClass());
 
 		assertEquals(ID, response.get(INDEX).getId());
@@ -107,7 +111,7 @@ class UserServiceImplTest {
 	void testCreate() {
 		when(repository.save(any())).thenReturn(user);
 		User response = serviceImpl.create(userDTO);
-		
+
 		assertNotNull(response);
 		assertEquals(User.class, response.getClass());
 		assertEquals(ID, response.getId());
@@ -115,29 +119,65 @@ class UserServiceImplTest {
 		assertEquals(EMAIL, response.getEmail());
 		assertEquals(PASSWORD, response.getPassword());
 	}
-	
-	//testando o metodo de criar usuario e verificando se nao ha violacao de dados
+
+	// testando o metodo de criar usuario e verificando se nao ha violacao de dados
 	@Test
 	void testCreateWithDataIntegratyViolationException() {
 		when(repository.findByEmail(anyString())).thenReturn(optionalUser);
-		
+
 		try {
 			optionalUser.get().setId(2);
 			serviceImpl.create(userDTO);
 		} catch (Exception e) {
 			assertEquals(DataIntegrityViolationException.class, e.getClass());
-			assertEquals("E-mail já cadastrado em nosso sistema!", e.getMessage());
+			assertEquals(E_MAIL_JA_CADASTRADO_EM_NOSSO_SISTEMA, e.getMessage());
 		}
 	}
 
 	@Test
 	void testUpdate() {
-		fail("Not yet implemented");
+		when(repository.save(any())).thenReturn(user);
+		User response = serviceImpl.update(userDTO);
+
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+		assertEquals(PASSWORD, response.getPassword());
+	}
+
+	@Test
+	void testUpdateWithDataIntegratyViolationException() {
+		when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+		try {
+			optionalUser.get().setId(2);
+			serviceImpl.create(userDTO);
+		} catch (Exception e) {
+			assertEquals(DataIntegrityViolationException.class, e.getClass());
+			assertEquals(E_MAIL_JA_CADASTRADO_EM_NOSSO_SISTEMA, e.getMessage());
+		}
 	}
 
 	@Test
 	void testDelete() {
-		fail("Not yet implemented");
+		when(repository.findById(anyInt())).thenReturn(optionalUser);
+		doNothing().when(repository).deleteById(anyInt());
+		serviceImpl.delete(ID);
+		verify(repository, times(1)).deleteById(anyInt());
+	}
+
+	@Test
+	void deleteWithObjectNotFoundException() {
+		when(repository.findById(anyInt())).thenThrow(
+				new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+		try {
+			serviceImpl.delete(ID);
+		} catch (Exception e) {
+			assertEquals(ObjectNotFoundException.class, e.getClass());
+			assertEquals(OBJETO_NAO_ENCONTRADO, e.getMessage());
+		}
 	}
 
 	private void startUser() {
